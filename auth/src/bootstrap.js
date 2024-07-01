2414aace131f3edbcb59a566a39bbfe8bd5fffeb
Inu(フェニックス) <dasujoy1999@gmail.com>
2024-07-01 21:08:18 +0530

    public path added

---
+++ auth/src/bootstrap.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createMemoryHistory } from "history";
+import { createBrowserHistory } from "history/cjs/history";
+
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) history.push(nextPathname);
+    },
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const ele = document.querySelector("#auth-dev-root");
+
+  if (ele) {
+    mount(ele, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
